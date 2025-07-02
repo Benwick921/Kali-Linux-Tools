4012301d54d1e055addc1d035187b49d8762ac3e
Benwick921 <44864440+Benwick921@users.noreply.github.com>
2025-07-02 09:38:22 +0100

    Add files via upload

--- .config/polybar/network.sh
+++ .config/polybar/network.sh
@@ -2,7 +2,7 @@
 
 while true; do
 	INT=$(ip link show | awk '{print $2}' | sed -n 'p;n' | sed 's/.$//' | tail -n+2)
-	INT=$(echo $INT | awk 'print $1}' | head -n 1
+	INT=$(echo $INT | awk '{print $1}' | head -n 1)
 	echo $(ip a | grep $INT | tail -n 1 | awk '{print $2}' | cut -d '/' -f 1)
 	sleep 5
 done
