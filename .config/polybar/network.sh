affbbfa04f6e58e1f0de8e95f6824cf93ebc0a2c
Benwick921 <44864440+Benwick921@users.noreply.github.com>
2025-07-02 09:14:33 +0100

    Updated now takes the IP correctly

--- .config/polybar/network.sh
+++ .config/polybar/network.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/bash
 
 while true; do
-	TUN0=$(ip a | grep tun0 | tail -n 1 | awk '{print $2}' | cut -d '/' -f 1)
-	ETH0=$(ip a | grep eth0 | tail -n 1 | awk '{print $2}' | cut -d '/' -f 1)
-
-	if [[ -z $TUN0 ]]; then
-		echo "$ETH0"
-	else
-		echo "$TUN0"
-	fi
+	INT=$(ip link show | awk '{print $2}' | sed -n 'p;n' | sed 's/.$//' | tail -n+2)
+	INT=$(echo $INT | awk 'print $1}' | head -n 1
+	echo $(ip a | grep $INT | tail -n 1 | awk '{print $2}' | cut -d '/' -f 1)
 	sleep 5
 done
 
